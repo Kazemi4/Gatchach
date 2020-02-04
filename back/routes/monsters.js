dd6418ececb6d75fae388bb94397f7769c2f499c
Nelo <kevinpplle@gmail.com>
2020-02-04 12:01:00 +0100

    fixed routes

--- back/routes/monsters.js
+++ back/routes/monsters.js
@@ -2,5 +2,13 @@ const router = require('express').Router()
 const connection = require("../config")
 
 router.get("/", (req, res) => {
-    connection.query("SELECT * FROM Monster", (err, results) =>
+    connection.query("SELECT * FROM Monster", (err, results) => {
+        if (err) {
+            res
+                .status(500)
+                .send(`erreur lors de la selection de la section dassault lol fdp`)
+        } else {
+            res.status(200).json(results)
+        }
+    })
 })
